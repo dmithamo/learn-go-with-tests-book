bfaab39a5314f9be0c68be633f1c386d24bae25e
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-09-23 08:41:00 +0300

    develop variadic integer adder pkg with tdd

---
+++ integers/integers.go
@@ -0,0 +1,11 @@
+package integers
+
+func Adder(nums ...int) int {
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
--- integers/integers_test.go
+++ integers/integers_test.go
@@ -1,1 +1,26 @@
 package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	t.Run("handles multiple  numbers", func(t *testing.T) {
+		got := Adder(2, 2, 2)
+		expected := 6
+		assetCorrectMsg(t, got, expected)
+	})
+}
+
+func assetCorrectMsg(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func ExampleAdder() {
+	fmt.Println(Adder(2, 2))
+	// Output 6
+}
