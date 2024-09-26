9df3ad51d991d6fa7b3ab95005634893b2a6882c
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-09-26 12:32:51 +0300

    develop arrays summing pkg with tdd

--- arrays_and_slices/arrays_and_slices.go
+++ arrays_and_slices/arrays_and_slices.go
@@ -1,9 +1,37 @@
 package arraysandslices
 
+// SumMembers returns the sum of the members of a slice
 func SumMembers(members []int) int {
-	sum := 0
-	for _, member := range members {
-		sum += member
-	}
-	return sum
+    if len(members) == 0 {
+        return 0
+    }
+
+    sum := 0
+    for _, member := range members {
+        sum += member
+    }
+    return sum
+}
+
+// MultiSliceSum adds members of each slice and returns slice with sums
+func MultiSliceSum(slices [][]int) []int {
+    sums := make([]int, 0, len(slices))
+    for _, slice := range slices {
+        sums = append(sums, SumMembers(slice))
+    }
+
+    return sums
+}
+
+func MultiSliceSumTails(slices [][]int) []int {
+    sums := make([]int, 0, len(slices))
+    for _, slice := range slices {
+        head := 0
+        if len(slice) > 0 {
+            head = slice[0]
+        }
+        sums = append(sums, SumMembers(slice)-head)
+    }
+
+    return sums
 }
--- arrays_and_slices/arrays_and_slices_test.go
+++ arrays_and_slices/arrays_and_slices_test.go
@@ -1,16 +1,50 @@
 package arraysandslices
 
 import (
-    testinghelpers "learninggowithtests/helpers"
-    "testing"
+	testinghelpers "learninggowithtests/helpers"
+	"testing"
 )
 
 func TestSumMembers(t *testing.T) {
-    t.Run("slice with any number of members", func(t *testing.T) {
-        given := []int{1, 2, 3, 4, 5}
-        want := 15
-        got := SumMembers(given)
+	t.Run("slice with any number of members", func(t *testing.T) {
+		given := []int{1, 2, 3, 4, 5}
+		want := 15
+		got := SumMembers(given)
 
-        testinghelpers.AssetCorrectMsg(t, got, want, given)
-    })
+		testinghelpers.AssetCorrectMsg(t, got, want, given)
+	})
+}
+
+func TestMultiSliceSums(t *testing.T) {
+	t.Run("several slices", func(t *testing.T) {
+		given := [][]int{[]int{1, 2, 3, 4, 5}, []int{2, 3, 4, 5, 6}}
+		want := []int{15, 20}
+		got := MultiSliceSum(given)
+		testinghelpers.AssetCorrectMsg(t, got, want, given)
+	})
+
+	// Test is unimportant - does not affect coverage, hence its
+	//   concerns are already addressed in the above test
+	// t.Run("single slice", func(t *testing.T) {
+	// 	given := [][]int{[]int{1, 2, 3, 4, 5}}
+	// 	want := []int{15}
+	// 	got := MultiSliceSum(given)
+	// 	testinghelpers.AssetCorrectMsg(t, got, want, given)
+	// })
+}
+
+func TestMultiSliceSumTails(t *testing.T) {
+	given := [][]int{[]int{1, 2, 3, 4, 5}, []int{2, 3, 4, 5, 6}, []int{1, 2}, []int{0, 9}}
+	want := []int{14, 18, 2, 9}
+	t.Run("handles several non-empty slices", func(t *testing.T) {
+		got := MultiSliceSumTails(given)
+		testinghelpers.AssetCorrectMsg(t, got, want, given)
+	})
+
+	t.Run("handles empty slices", func(t *testing.T) {
+		given = append(given, []int{})
+		want = append(want, 0)
+		got := MultiSliceSumTails(given)
+		testinghelpers.AssetCorrectMsg(t, got, want, given)
+	})
 }
